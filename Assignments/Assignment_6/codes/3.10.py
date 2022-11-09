9cf8e1f7bad09974cae5a0ac857696c076271f86
Karthik Kurugodu <77197301+kurugodukarthik11@users.noreply.github.com>
2022-11-09 12:45:49 +0530

    Update 3.10.py

--- Assignments/Assignment_6/codes/3.10.py
+++ Assignments/Assignment_6/codes/3.10.py
@@ -25,6 +25,6 @@ plt.xlabel('f (Hz)')
 plt.ylabel('H(f)')
 plt.savefig('../figs/3.10.pdf')
 plt.show()
-import shellx
+import shlex
 import subprocess
 subprocess.run(shlex.split("termux-open ../figs/3.10.pdf"))
