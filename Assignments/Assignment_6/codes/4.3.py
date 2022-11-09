0b4ecbb673ef7ee73b17a99c3bd8876b58c6cb79
Karthik Kurugodu <77197301+kurugodukarthik11@users.noreply.github.com>
2022-11-09 12:46:47 +0530

    Update 4.3.py

--- Assignments/Assignment_6/codes/4.3.py
+++ Assignments/Assignment_6/codes/4.3.py
@@ -17,6 +17,6 @@ plt.xlabel('t (s)')
 plt.ylabel('V')
 plt.savefig('../figs/4.3.pdf')
 plt.show()
-import shellx
+import shlex
 import subprocess
 subprocess.run(shlex.split("termux-open ../figs/4.3.pdf"))
