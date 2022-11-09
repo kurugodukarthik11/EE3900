e11c78c854e0dd010d1ff56fb1a4711c962ce05a
Karthik Kurugodu <77197301+kurugodukarthik11@users.noreply.github.com>
2022-11-09 12:39:17 +0530

    Update 5.4.py

--- Assignments/Assignment_6/codes/5.4.py
+++ Assignments/Assignment_6/codes/5.4.py
@@ -14,3 +14,6 @@ plt.grid()
 plt.legend(['Simulation', 'Analysis'])
 plt.savefig('../figs/5.4.pdf')
 plt.show()
+import shellx
+import subprocess
+subprocess.run(shlex.split("termux-open ../figs/5.4.pdf"))
